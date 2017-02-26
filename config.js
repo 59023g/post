30f09aca40413f8987513913f9663c54f6935e86
Michael Pierce <m.edward.pierce@gmail.com>
2017-02-25 18:12:08 -0800

    config: DB_PATH, PORT

---
+++ config.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+const CONFIG_PATH = ''
+
+module.exports = {
+  APP_NAME: 'blog',
+
+  CONFIG_PATH: CONFIG_PATH,
+  DB_PATH: ('./db'),
+  KEYS_PATH: path.join(CONFIG_PATH, 'public-keys'),
+  PORT: 8000
+
+}
