7ce0d32b6ccf1fabddfb1e01996919dfb753e8c1
Michael Pierce <m.edward.pierce@gmail.com>
2017-03-07 10:31:17 -0800

    src/routes: build out form, list view, index stream

--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,38 @@ const db = require( '../db.js' )
 
 module.exports = async function routes( path, req, res ) {
   console.log( 'path: ', path )
+  console.log( /^\/post\/./.test(path) )
 
   switch ( path ) {
     case '/':
 
-      let parsedData =[]
+      let dataStreamArr =[]
 
        db.createReadStream({ reverse: true })
-       .on('data', function (data) {
-          parsedData.push( data )
+       .on('data', function ( data ) {
+
+         let dataValue = data.value.data
+
+         if ( !data.value.data.author ) {
+           dataValue.author = null;
+         }
+
+         if ( !dataValue.subject ) {
+           dataValue.subject = null;
+         }
+
+         if ( !dataValue.body ) {
+           dataValue.body = null;
+         }
+
+         let processData = {
+           author: dataValue.author,
+           postDate: data.key.split('!')[1],
+           subject: dataValue.subject,
+           body: dataValue.body
+         }
+
+         dataStreamArr.push( processData )
         })
         .on('error', function (err) {
           console.log('Oh my!', err)
@@ -19,11 +42,14 @@ module.exports = async function routes( path, req, res ) {
         .on('close', function () {
           console.log('Stream closed')
         })
-        .on('end', function (data) {
+        .on('end', function () {
           console.log('Stream ended')
-          send( res, 200, data )
+            view( req, res, dataStreamArr )
 
         })
+      break;
+    case ( /^\/post\/./.test(path) ):
+      console.log('match')
       break;
     case '/admin':
       break;
@@ -45,11 +71,52 @@ async function post( req, res ) {
     `
       ${ getHead() }
         <form method='post' action='/admin/post'>
-          <input type='text' name='subject'>
-          <input type='submit' value='submit'>
+          <div>
+            subj
+            <input type='text' name='subject'>
+          </div>
+          <div>
+            body
+            <textarea name='body'></textarea>
+          </div>
+          <div>
+            subm
+            <input type='submit' value='submit'>
+          </div>
         </form>
       ${ getFoot() }
+      `
 
+
+   await send( res, 200, form )
+}
+
+async function view( req, res, data ) {
+
+  let list = []
+
+  for (var i = 0; i < data.length; i++) {
+      list.push(
+        `<li>
+          <h4>Author: ${ data[i].author } </h4>
+          __
+          <h5>Subject: ${ data[i].subject  } </h5>
+          __
+          <p>Body: ${ data[i].body  } </p>
+          __
+          <p><a href="post/${ data[i].postDate }">${  data[i].postDate   }</a></p>
+        </li>
+        `
+      )
+  }
+
+  let form =
+      `
+        ${ getHead() }
+        <ul>
+          ${ list.join('') }
+        </ul>
+        ${ getFoot() }
       `
 
 
@@ -61,7 +128,11 @@ async function post( req, res ) {
     `
     <html>
     <head>
+      <style>
+        body { font-family: monospace }
+        div, .padding-bottom-8 { padding-bottom: 8px }
 
+      </style>
     </head>
     <body>
 
