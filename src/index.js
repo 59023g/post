23d8c607f2ccf79cc899e8866e7ac49b14c54655
59023g <hi@mep.im>
2017-03-29 09:50:05 -0700

    src/index: getPosts(), w filter

--- src/index.js
+++ src/index.js
@@ -45,6 +45,76 @@ app.get( '/admin/post', ( req, res ) => {
 
 app.post( '/admin/post', ( req, res ) => {
 
+let getPosts = async () => {
+
+  // get a key stream
+  // check if existing key exists
+  // if not, push to allKeys
+  // if so, split the key and check which is newer
+  //     and push new key (not split) to allKeys
+  //     and remove old key from allKeys
+  // then lookup data for keys
+  // render layout
+
+  let allKeys = []
+
+  return new Promise( ( resolve, reject ) => {
+
+    // this could be a key stream, but figured it'd be faster to include the body too, at least for now
+    db.createReadStream()
+      .on( 'data', function( item ) {
+
+        console.log( item )
+
+        let keyObj = util.splitKey( item.key )
+
+        // get last key put into array
+        let lastKey = util.lastArr( allKeys );
+
+        if ( lastKey ) {
+
+          lastKey = lastKey.key.split( config.splitValue );
+          /*
+            check if lastkey.createdAt == the newKey.createdAt -
+            this means there may be an updated item
+          */
+          if ( lastKey[ 0 ] === keyObj.createdAt ) {
+
+            // check if newKey updatedAt is newer in time
+            if ( lastKey[ 1 ] < keyObj.updatedAt ) {
+
+              // remove lastKey
+              allKeys.pop();
+              /*
+                push newKey because it's newer and we don't want to display
+                all edits on a page like index
+              */
+              allKeys.push( item )
+
+            }
+
+          } else {
+            // if the item does not already exist, then push it
+            allKeys.push( item );
+
+          }
+
+          // if there's no lastKey, new push the item
+        } else {
+          allKeys.push( item );
+
+        }
+
+      } )
+      .on( 'error', function( err ) {
+        reject( err )
+      } )
+      .on( 'end', function() {
+        resolve( allKeys )
+      } )
+
+  } )
+}
   let content = req.body;
   let author = 'mp';
   let now = Date.now();
