758dfd0160a685b8fd403c14a0cf7539e29c77e5
59023g <hi@mep.im>
2017-03-29 09:48:54 -0700

    config: add db splitValue = '+'

--- config.js
+++ config.js
@@ -4,10 +4,10 @@ const CONFIG_PATH = ''
 
 module.exports = {
   APP_NAME: 'blog',
-
   CONFIG_PATH: CONFIG_PATH,
   DB_PATH: ('./db'),
   KEYS_PATH: path.join(CONFIG_PATH, 'public-keys'),
-  PORT: 8000
+  PORT: 8000,
+  splitValue: '+'
 
 }
