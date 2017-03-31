caa6d2b332bcfda98e6d374f3a5f6552697bea8e
59023g <hi@mep.im>
2017-03-31 11:57:59 -0700

    db: use sublevel, invoke usersDb and itemsDb

--- db.js
+++ db.js
@@ -1,15 +1,21 @@
 const config = require( './config.js' )
 
 // database
-const level = require( 'level' )
+const Level = require( 'level' )
+const SubLevel = require( 'level-sublevel' )
+
 // const hyperlog = require( 'hyperlog' )
 
 
-let db = level( config.DB_PATH, { valueEncoding: 'json' } )
+let db = SubLevel( Level( config.DB_PATH, { valueEncoding: 'json' } ) );
+
+let usersDb = db.sublevel( 'users' );
+let itemsDb = db.sublevel( 'items' );
 
 // thought about a separate db, but i think for replicability, keep it in but encrypt the object
 // let userDb = level( config.USER_DB_PATH, { valueEncoding: 'json' } )
 
 module.exports = {
-  db: db
+  usersDb: usersDb,
+  itemsDb: itemsDb
 }
