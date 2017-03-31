a4a3964f371fe95c6522540dd4df3394fa0a3c11
59023g <hi@mep.im>
2017-03-31 09:50:23 -0700

    config: fake user_db

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ module.exports = {
   APP_NAME: 'blog',
   CONFIG_PATH: CONFIG_PATH,
   DB_PATH: ('./db'),
+  USER_DB_PATH: ('./user_db'),
   KEYS_PATH: path.join(CONFIG_PATH, 'public-keys'),
   PORT: 8000,
   splitValue: '+'
