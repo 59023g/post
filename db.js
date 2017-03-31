c65b04ab30af268c565c11a3d654ef77b55cfe6a
59023g <hi@mep.im>
2017-03-31 09:51:04 -0700

    db: fake userDb

--- db.js
+++ db.js
@@ -7,4 +7,9 @@ const level = require( 'level' )
 
 let db = level( config.DB_PATH, { valueEncoding: 'json' } )
 
-module.exports = db
+// thought about a separate db, but i think for replicability, keep it in but encrypt the object
+// let userDb = level( config.USER_DB_PATH, { valueEncoding: 'json' } )
+
+module.exports = {
+  db: db
+}
