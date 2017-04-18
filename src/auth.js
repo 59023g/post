f643d96742c0d86f7be4bcd9420fa50477a87b0a
59023g <hi@mep.im>
2017-04-18 12:48:54 -0700

    src/auth: verifyToken(), getToken()

--- src/auth.js
+++ src/auth.js
@@ -1,10 +1,51 @@
-// const db = require( '../db_interactor.js' )
-const config = require( '../config.js' );
-const util = require( './util.js' );
+const jwt = require( 'jsonwebtoken' )
 
+// const db = require( '../db_interactor.js' )
+const util = require( './util.js' );
 const usersDb = require( '../db.js' ).usersDb
 
-const secrets = require( '../secrets.js' );
+function getToken(req) {
+  if (req.headers.authorization &&
+      req.headers.authorization.split(' ')[0] === 'Bearer') {
+        return req.headers.authorization.split(' ')[1];
+  }
+  return null;
+}
+
+const verifyToken = ( req, res, next ) => {
+
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers[ 'x-access-token' ];
+
+  // decode token
+  if ( token ) {
+
+    // verifies secret and checks exp
+    jwt.verify( token, process.env.DEV_KEY, function( err, decoded ) {
+      if ( err ) {
+        return res.json( {
+          success: false,
+          message: 'Failed to authenticate token.'
+        } );
+      } else {
+        // if everything is good, save to request for use in other routes
+        console.log( 'sucess', decoded )
+        req.decoded = decoded;
+        next();
+      }
+    } );
+
+  } else {
+
+    // if there is no token
+    // return an error
+    return res.status( 403 ).send( {
+      success: false,
+      message: 'No token provided.'
+    } );
+
+  }
+};
 
 const deleteUser = async( req, res ) => {
   // let key =
@@ -97,10 +138,20 @@ const login = async( req, res ) => {
       if ( latestAuthorItem.pass === pass ) {
         // latest user item password matches
         // send token to client
-        console.log('success')
+        jwt.sign( {
+          foo: 'bar'
+        }, process.env.DEV_KEY, {
+          algorithm: 'HS256'
+        }, function( err, token ) {
+          res.json( {
+            success: true,
+            message: 'token',
+            token: token
+          } )
+        } );
       } else {
         // latest user item password does not match
-        console.log( 'fail')
+        console.log( 'fail' )
       }
 
 
@@ -118,5 +169,6 @@ const login = async( req, res ) => {
 module.exports = {
   login: login,
   createUser: createUser,
-  deleteUser: deleteUser
+  deleteUser: deleteUser,
+  verifyToken: verifyToken
 }
