c92c5b85ebf0d6a49485aedf16959206f810506d
59023g <hi@mep.im>
2017-04-20 12:18:15 -0700

    src/util: lastArr() -> lastArrItem()

--- src/util.js
+++ src/util.js
@@ -1,16 +1,14 @@
-const lastArr = ( arr ) => {
+const lastArrItem = ( arr ) => {
   return arr[arr.length - 1 ];
 }
 
 const splitKey = ( key ) => {
   let keyArr = key.split( process.env.SPLIT_VALUE );
-
   let keyObj = {
     createdAt: keyArr[ 0 ],
     updatedAt: keyArr[ 1 ],
     author: keyArr[ 2 ]
   };
-
   return keyObj
 }
 
@@ -19,7 +17,7 @@ const sortDescUpdatedAt = ( a, b ) => {
 }
 
 module.exports = {
-  lastArr: lastArr,
+  lastArrItem: lastArrItem,
   sortDescUpdatedAt: sortDescUpdatedAt,
   splitKey: splitKey
 }
