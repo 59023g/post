84aa717744991a25b628175e7cc713e59062c5c8
59023g <hi@mep.im>
2017-04-20 12:17:03 -0700

    db.js: config -> process.env

--- db.js
+++ db.js
@@ -1,5 +1,3 @@
-const config = require( './config.js' )
-
 // database
 const Level = require( 'level' )
 const SubLevel = require( 'level-sublevel' )
@@ -7,13 +5,13 @@ const SubLevel = require( 'level-sublevel' )
 // const hyperlog = require( 'hyperlog' )
 
 
-let db = SubLevel( Level( config.DB_PATH, { valueEncoding: 'json' } ) );
+let db = SubLevel( Level( process.env.DB_PATH, { valueEncoding: 'json' } ) );
 
 let usersDb = db.sublevel( 'users' );
 let itemsDb = db.sublevel( 'items' );
 
 // thought about a separate db, but i think for replicability, keep it in but encrypt the object
-// let userDb = level( config.USER_DB_PATH, { valueEncoding: 'json' } )
+// let userDb = level( process.env.USER_DB_PATH, { valueEncoding: 'json' } )
 
 module.exports = {
   usersDb: usersDb,
